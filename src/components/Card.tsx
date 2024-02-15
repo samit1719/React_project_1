11200f7abb0893dfbf25bfcb6df6901db3ed75dd
samit1719 <samuelteshome1719@gmail.com>
2024-02-15 23:36:43 +0300

    working on examples and blog page

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,53 +1,80 @@
 import styled from "styled-components";
 
 const CardContainer = styled.div`
-  width: 600px;
+  width: 500px;
+  height: 500px;
   border-radius: 32px;
   background-color: #fff;
   overflow: hidden;
   margin: 32px;
   box-shadow: 0 100px 140px -50px rgba(0, 0, 0, 0.25),
     0 0 20px 0 rgba(0, 0, 0, 0.03);
-  cursor: pointer;
+  cursor: default;
+  transition: transform 0.5s ease-in-out;
+
+  &:hover {
+    transform: scale(1.05, 1.05);
+    cursor: pointer;
+  }
 `;
 
-const VideoContainer = styled.video``;
-const ImageContainer = styled.img``;
-const TextContainer = styled.div``;
-const CardTitle = styled.h3``;
+const CardHero = styled.div`
+  width: 100%;
+  height: 50%;
+`;
+const VideoContainer = styled.video`
+  height: auto;
+  width: 100%;
+  object-fit: cover;
+`;
+const ImageContainer = styled.img`
+  height: auto;
+  width: 100%;
+  object-fit: cover;
+`;
+const TextContainer = styled.div`
+  height: 50%;
+  padding: 1em 2em 2em 2em;
+  font-size: 1.1em;
+`;
+const CardTitle = styled.h3<{ $cardDate: boolean }>`
+  margin-top: ${(prop) => (prop.$cardDate ? "2em" : "4em")};
+`;
 const CardText = styled.p``;
-const CardDate = styled.p``;
+const CardDateContainer = styled.div``;
+const CardDate = styled.p`
+  font-size: 0.75em;
+  color: #989898;
+`;
 
 const Card = (props: {
-  isExample?: boolean;
   videoUrl?: string;
   imageUrl?: string;
-  cardTitle?: string;
-  cardText?: string;
+  cardTitle: string;
+  cardText: string;
   cardDate?: string;
   altText: string;
 }) => {
-  const {
-    isExample = false,
-    videoUrl,
-    imageUrl,
-    cardText,
-    cardTitle,
-    cardDate,
-    altText,
-  } = props;
+  const { videoUrl, imageUrl, cardText, cardTitle, cardDate, altText } = props;
 
   return (
     <CardContainer>
-      {isExample ? (
-        <VideoContainer src={videoUrl} />
-      ) : (
-        <ImageContainer src={imageUrl} alt={altText} />
-      )}
+      <CardHero>
+        {videoUrl ? (
+          <VideoContainer src={videoUrl} autoPlay loop />
+        ) : (
+          <ImageContainer src={imageUrl} alt={altText} />
+        )}
+      </CardHero>
+
       <TextContainer>
-        <CardTitle>{cardTitle}</CardTitle>
+        <CardTitle $cardDate={!!cardDate}>{cardTitle}</CardTitle>
         <CardText>{cardText}</CardText>
-        {!isExample && <CardDate>{cardDate}</CardDate>}
+        {cardDate && (
+          <CardDateContainer>
+            <CardDate>Release&nbsp;&nbsp;·&nbsp;&nbsp;{cardDate}</CardDate>
+          </CardDateContainer>
+        )}
       </TextContainer>
     </CardContainer>
   );
--- src/pages/Examples.tsx
+++ src/pages/Examples.tsx
@@ -1,21 +1,72 @@
-import Layout from '../components/Layout'
-import styled from 'styled-components'
+import styled from "styled-components";
+import Layout from "../components/Layout";
+import Card from "../components/Card";
 
 const ExamplesContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
   flex-wrap: wrap;
 `;
 
+type Example = {
+  videoUrl: string;
+  title: string;
+  subTitle: string;
+};
+
 const Examples = () => {
+  const examples = [
+    {
+      videoUrl: "https://www.lobe.ai/assets/examples/Plants.mp4",
+      title: "Plant Species",
+      subTitle:
+        "Train your app to distinguish between different species of plants to help people identify poisonous vegetation.",
+    },
+    {
+      videoUrl: "https://www.lobe.ai/assets/examples/Hand%20Count.mp4",
+      title: "Hand Gestures",
+      subTitle:
+        "Train your app to understand the movement of your hand to create new kinds of immersive user experiences.",
+    },
+    {
+      videoUrl: "https://www.lobe.ai/assets/examples/Feel%20the%20Burn.mp4",
+      title: "Personal Trainer",
+      subTitle:
+        "Train your app to recognize different workout positions to create an automated personal trainer that counts your reps.",
+    },
+    {
+      videoUrl: "https://www.lobe.ai/assets/examples/Laughing.mp4",
+      title: "Emotional Reactions",
+      subTitle:
+        "Train your app to react to different expressions allowing people to send emoji reactions using just their face.",
+    },
+    {
+      videoUrl:
+        "https://www.lobe.ai/assets/examples/Painting%20with%20Mike.mp4",
+      title: "Interactive Painting",
+      subTitle:
+        "Train your app to respond with sounds and stories while kids draw to create engaging learning experiences.",
+    },
+  ];
+
   return (
     <Layout>
       <ExamplesContainer>
-        {/* Insert your code here */}
+        {examples.map((example: Example, idx: number) => {
+          return (
+            <Card
+              videoUrl={example.videoUrl}
+              cardTitle={example.title}
+              cardText={example.subTitle}
+              altText={example.title}
+              key={idx}
+            />
+          );
+        })}
       </ExamplesContainer>
     </Layout>
-  )
-}
+  );
+};
 
-export default Examples
+export default Examples;
